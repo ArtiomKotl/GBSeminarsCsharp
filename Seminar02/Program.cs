147043943b7c6206b44276f405fb74a8dab841a3
artketler <artketler.ak@gmail.com>
2023-02-17 22:08:47 +0300

    Examples003

--- Seminar02/Program.cs
+++ Seminar02/Program.cs
@@ -1,18 +1,26 @@
 ﻿/*
-Написать программу, которая генерирует случайное
-трёхзначное число
-и удаляет вторую цифру этого числа.
-456 -> 46
-782 -> 72
-918 -> 98
+Написать программу, которая будет принимать на вход два числа и выводить,
+является ли второе число кратным первому.
+Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+34, 5 -> Не кратно, остаток 4
+16, 4 -> Кратно
 */
 
-Console.WriteLine("Программа генерирует случайное трёхзначное целое положительное число, после чего удаляет из него вторую цифру и выводит полученный результат на экран.");
-int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine("Задайте два числа. Программа проверит, является ли второе число кратным для первого числа.");
+Console.Write("Введите первое число = ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число = ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
 
-int firstDigit = randomNumber / 100 * 10;
-int secondDigit = randomNumber % 10;
-int resultNumber = firstDigit + secondDigit;
+if (numberTwo == 0)
+{
+    Console.WriteLine("Ошибка! Невозможно произвести деления на ноль!");
+    return;
+}
 
-Console.WriteLine("Сгенерированное число = " + randomNumber);
-Console.WriteLine("Результирующее число = " + resultNumber);
+int result = numberOne % numberTwo;
+
+if (result == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine("Не кратно. Остаток от деления = " + result);
