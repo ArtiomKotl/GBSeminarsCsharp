d3ca97d6df37988c109c5a898953fd5d747a3f6b
artketler <artketler.ak@gmail.com>
2023-02-20 23:28:02 +0300

    Examples003

--- Seminar03/Program.cs
+++ Seminar03/Program.cs
@@ -1,40 +1,25 @@
 ﻿/*
-Написать программу, которая по заданному номеру четверти плоскости
-показывает возможный диапазон координат в этой четверти.
+Написать программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 2D пространстве
+A(3, 6); B(2, 1) -> 5,09
+A(7, 5); B(1, -1) -> 7,21
 */
 
-int CoordinatesX(int number){
-    if ((number == 1) || (number == 4))
-        return 1;
-    else if ((number == 2) || (number == 3))
-        return -1;
-    return 0;
+double Distance(double ax, double ay, double bx, double by)
+{
+    return Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2));
 }
 
-int CoordinatesY(int number){
-    if ((number == 1) || (number == 2))
-        return 1;
-    else if ((number == 3) || (number == 4))
-        return -1;
-    return 0;
-}
+Console.WriteLine("Программа показывает расстояние между двумя точкам на плоскости по введённым координатам.");
+Console.Write("Введите координату X точка A = ");
+double ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точка A = ");
+double ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату X точка B = ");
+double bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точка B = ");
+double by = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Программа показывает возможный диапазон координат по введённому номеру четверти плоскости.");
-Console.Write("Введите номер четверти плоскости: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if ((num != 1) && (num != 2) && (num != 3) && (num != 4)){
-    Console.WriteLine($"Введённого номера плоскости {num} не существует. Программа прервана");
-    return;
-}
+double res = Distance(ax, ay, bx, by);
 
-int x = CoordinatesX(num);
-int y = CoordinatesY(num);
-
-if ((x == 1) && (y == 1))
-    Console.WriteLine($"Координаты {num} четверти: X от {x} до плюс бесконечности, Y от {y} до плюс бесконечности");
-else if ((x == -1) && (y == 1))
-    Console.WriteLine($"Координаты {num} четверти: X от {x} до минус бесконечности, Y от {y} до плюс бесконечности");
-else if ((x == -1) && (y == -1))
-    Console.WriteLine($"Координаты {num} четверти: X от {x} до минус бесконечности, Y от {y} до минус бесконечности");
-else if ((x == 1) && (y == -1))
-    Console.WriteLine($"Координаты {num} четверти: X от {x} до плюс бесконечности, Y от {y} до минус бесконечности");
+Console.WriteLine($"Расстояние между точками A({ax}, {ay}) и B({bx}, {by}) = {Math.Round(res, 4)}");
